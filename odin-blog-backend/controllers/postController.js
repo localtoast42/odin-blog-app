f6a3314d1cec8c54f6a7f5db14f5351e69b7bdaf
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-09 12:27:42 -0400

    Implement postController functions

--- odin-blog-backend/controllers/postController.js
+++ odin-blog-backend/controllers/postController.js
@@ -1,11 +1,85 @@
 const Post = require("../models/post");
+const Comment = require("../models/comment");
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
-exports.post_list_get = {};
+exports.post_list_get = asyncHandler(async (req, res, next) => {
+    const allPosts = await Post.find()
+        .sort({ timestamp: 1 })
+        .populate("author")
+        .exec();
+  
+    res.render("post_list", { title: "Posts", user: req.user, post_list: allPosts });
+});
 
-exports.post_create = [];
+exports.post_create = [
+    body("title")
+        .trim()
+        .isLength( { min: 1 })
+        .escape()
+        .withMessage("Title must be provided."),
+    body("text")
+        .trim()
+        .isLength( { min: 1 })
+        .escape()
+        .withMessage("Post must not be empty."),
 
-exports.post_update = [];
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
 
-exports.post_delete = {};
+        const post = new Post({
+            title: req.body.title,
+            author: req.user.id,
+            text: req.body.text,
+        });
+
+        if (!errors.isEmpty()) {
+            return;
+        } else {
+            await post.save();
+            res.redirect('/');
+        };
+    }),
+];
+
+exports.post_update = [
+    body("title")
+        .trim()
+        .isLength( { min: 1 })
+        .escape()
+        .withMessage("Title must be provided."),
+    body("text")
+        .trim()
+        .isLength( { min: 1 })
+        .escape()
+        .withMessage("Post must not be empty."),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const post = new Post({
+            _id: req.params.id,
+            title: req.body.title,
+            author: req.user.id,
+            text: req.body.text,
+        });
+
+        if (req.body.isPublished) {
+            post.isPublished = true;
+            post.publishedDate = Date.now();
+        };
+
+        if (!errors.isEmpty()) {
+            return;
+        } else {
+            const updatedPost = await Post.findByIdAndUpdate(req.params.id, post, {});
+            res.redirect(updatedPost.url);
+        };
+    }),
+];
+
+exports.post_delete = asyncHandler(async (req, res, next) => {    
+    await Comment.deleteMany({ post: req.params.id });
+    await Post.findByIdAndDelete(req.params.id);
+    res.redirect("/posts");
+});

df3b235c99954cd439853e18aaf5fbed1a941b75
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-09 12:35:28 -0400

    Add config middlewares to app

--- odin-blog-backend/app.js
+++ odin-blog-backend/app.js
@@ -1,12 +1,17 @@
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
+
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,6 +21,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1', indexRouter);

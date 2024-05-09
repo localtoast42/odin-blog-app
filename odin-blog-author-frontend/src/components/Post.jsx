755176e170e0b10878217cc2f1d53af6bb6c9776
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-09 15:31:07 -0400

    Implement Post component

--- odin-blog-author-frontend/src/components/Post.jsx
+++ odin-blog-author-frontend/src/components/Post.jsx
@@ -1,9 +1,25 @@
+import "./Post.css";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const Post = () => {
+    const [post, setPost] = useState([]);
+
+    const { postId } = useParams();
+
+    useEffect(() => {
+        fetch(`https://<tempurl>/api/v1/posts/${postId}`, { mode: "cors" })
+            .then((response) => response.json())
+            .then((response) => setPost(response))
+            .catch((error) => console.error(error));
+    }, [postId]);
 
     return (
-        <>
-        </>
+        <div className="post">
+            <h3 className="post-title">{post.title}</h3>
+            <h2 className="post-info">{post.author + ' ' + post.datePublished}</h2>
+            <p className="post-text">{post.text}</p>
+        </div>
     );
 };
 

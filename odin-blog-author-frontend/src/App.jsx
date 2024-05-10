88901044e98e9d58a6f4a7cf20f15a01e026abce
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-10 12:01:17 -0400

    Add navlinks to home view

--- odin-blog-author-frontend/src/App.jsx
+++ odin-blog-author-frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
-import { Outlet } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 
 function App() {
 
   return (
     <>
       <nav className='navbar'>
-
+        <NavLink>Home</NavLink>
+        <NavLink>Sign in</NavLink>
+        <NavLink>Sign out</NavLink>
       </nav>
       <div className='content'>
         <Outlet />

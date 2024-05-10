1a150f71214d129a9b3a4f3ee768c6794770ec07
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-10 13:20:14 -0400

    Add PostCard component and stylesheet

---
+++ odin-blog-author-frontend/src/components/PostCard.jsx
@@ -0,0 +1,20 @@
+import "./PostCard.css";
+import { NavLink } from "react-router-dom";
+import PropTypes from 'prop-types';
+
+const PostCard = ({ post }) => {
+
+    return (
+        <NavLink to={`posts/${post.id}`}>
+            <h3>{post.title}</h3>
+            <h2>{post.author}</h2>
+            {post.publishedDate ? <h2>{post.publishedDate}</h2> : false}
+        </NavLink>
+    );
+};
+
+PostCard.propTypes = {
+    post: PropTypes.object.isRequired,
+}
+
+export default PostCard;

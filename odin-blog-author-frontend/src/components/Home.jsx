7d5d3563b836143c4c26254bf4f6ab925e9c80a1
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-10 13:18:39 -0400

    Add content to Home component

--- odin-blog-author-frontend/src/components/Home.jsx
+++ odin-blog-author-frontend/src/components/Home.jsx
@@ -1,9 +1,13 @@
+import "./Home.css";
+import PostContainer from "./PostContainer";
 
 const Home = () => {
 
     return (
-        <>
-        </>
+        <div className="home-container">
+            <PostContainer></PostContainer>
+            <div className="sidebar"></div>
+        </div>
     );
 };
 

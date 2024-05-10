8bb3492f48df384f73e778fd02ed9b1aefd77adb
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-10 13:19:47 -0400

    Add content and postList API request

--- odin-blog-author-frontend/src/components/PostContainer.jsx
+++ odin-blog-author-frontend/src/components/PostContainer.jsx
@@ -1,9 +1,24 @@
+import "./PostContainer.css";
+import { useState, useEffect } from "react";
+import PostCard from "./PostCard";
 
 const PostContainer = () => {
+    const [postList, setPostList] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/api/v1/posts/`, { mode: "cors" })
+            .then((response) => response.json())
+            .then((response) => setPostList(response))
+            .catch((error) => console.error(error));
+    }, []);
 
     return (
-        <>
-        </>
+        <div className="post-container">
+            {postList.map(post => <PostCard 
+                key={post.id} 
+                post={post} />
+            )}
+        </div>
     );
 };
 

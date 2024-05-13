b1d06f7b614c2d1012a65dc573571176a72c1cee
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-13 17:25:55 -0400

    Add login form method, action attributes

--- odin-blog-author-frontend/src/components/Login.jsx
+++ odin-blog-author-frontend/src/components/Login.jsx
@@ -4,7 +4,7 @@ const Login = () => {
 
     return (
         <div className="login-card">
-            <form className="login-form" action="">
+            <form className="login-form" method="POST" action="http://localhost:3000/api/v1/login">
                 <input type="text" name="username" id="username" placeholder="Username"/>
                 <input type="password" name="password" id="password" placeholder="Password"/>
                 <button>Sign in</button>

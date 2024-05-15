1d330cd8898e35248763b55dca752ba11148234e
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-15 17:24:50 -0400

    Add content to sidebar

--- odin-blog-author-frontend/src/components/Home.jsx
+++ odin-blog-author-frontend/src/components/Home.jsx
@@ -6,7 +6,10 @@ const Home = () => {
     return (
         <div className="home-container">
             <PostContainer></PostContainer>
-            <div className="sidebar"></div>
+            <div className="sidebar">
+                <h2>Description</h2>
+                <p>This is my blog, check it out!</p>
+            </div>
         </div>
     );
 };

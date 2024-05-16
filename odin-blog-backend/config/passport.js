f42c88639aa925e81db12b13428ad1761aeacbd7
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-16 12:46:45 -0400

    Extract JWT from cookies instead of auth header

--- odin-blog-backend/config/passport.js
+++ odin-blog-backend/config/passport.js
@@ -2,13 +2,20 @@ const fs = require('fs');
 const path = require('path');
 const User = require('../models/user');
 const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 const pathToKey = path.join(__dirname, '..', 'id_rsa_pub.pem');
 const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
 
+const cookieExtractor = (req) => {
+    let token = null;
+    if (req && req.cookies) {
+        token = req.cookies['jwt'];
+    }
+    return token;
+};
+
 const options = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    jwtFromRequest: cookieExtractor,
     secretOrKey: PUB_KEY,
     algorithms: ['RS256'],
 };

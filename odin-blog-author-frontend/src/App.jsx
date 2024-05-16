572e6292ee296b3dbc51b0f995b9986435774008
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-16 13:55:45 -0400

    Request user info on app load

--- odin-blog-author-frontend/src/App.jsx
+++ odin-blog-author-frontend/src/App.jsx
@@ -1,14 +1,27 @@
 import './App.css'
+import { useState, useEffect } from "react";
 import { Outlet, NavLink } from "react-router-dom";
 
 function App() {
+
+    const [user, setUser] = useState();
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/api/v1/users/self`, { mode: "cors", credentials: "include" })
+            .then((response) => response.json())
+            .then((response) => setUser(response))
+            .catch((error) => console.error(error));
+    }, []);
 
     return (
         <>
             <nav className='navbar'>
                 <NavLink to="">Home</NavLink>
-                <NavLink to="login">Sign in</NavLink>
-                <a href="http://localhost:3000/api/v1/logout">Sign out</a>
+                {user ? (
+                    <a href="http://localhost:3000/api/v1/logout">Sign out</a>
+                ) : (
+                    <NavLink to="login">Sign in</NavLink>
+                )}
             </nav>
             <div className='content'>
                 <Outlet />

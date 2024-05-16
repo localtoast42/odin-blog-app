5f931569e2bcf53f7c003ad5ad10c62f5b3bfa87
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-16 13:09:26 -0400

    Change Sign Out link to backend logout endpoint

--- odin-blog-author-frontend/src/App.jsx
+++ odin-blog-author-frontend/src/App.jsx
@@ -3,18 +3,18 @@ import { Outlet, NavLink } from "react-router-dom";
 
 function App() {
 
-  return (
-    <>
-      <nav className='navbar'>
-        <NavLink to="">Home</NavLink>
-        <NavLink to="login">Sign in</NavLink>
-        <NavLink to="logout">Sign out</NavLink>
-      </nav>
-      <div className='content'>
-        <Outlet />
-      </div>
-    </>
-  )
+    return (
+        <>
+            <nav className='navbar'>
+                <NavLink to="">Home</NavLink>
+                <NavLink to="login">Sign in</NavLink>
+                <a href="http://localhost:3000/api/v1/logout">Sign out</a>
+            </nav>
+            <div className='content'>
+                <Outlet />
+            </div>
+        </>
+    )
 }
 
 export default App

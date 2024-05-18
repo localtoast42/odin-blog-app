6684f6d925731125a17cad580b55ab5a876502e9
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-18 15:06:36 -0400

    Add comment create action to new route

--- odin-blog-user-frontend/src/routes.jsx
+++ odin-blog-user-frontend/src/routes.jsx
@@ -1,11 +1,10 @@
 import App from "./App";
 import PostContainer from "./components/PostContainer";
 import Post from "./components/Post";
-import PostEdit from "./components/PostEdit";
 import Login from "./components/Login";
 import ErrorPage from "./components/ErrorPage";
 import { postLoader, postContainerLoader, userLoader } from "./loaders";
-import { postCreateAction, postUpdateAction, postDeleteAction } from "./actions";
+import { commentCreateAction } from "./actions";
 
 const routes = [
     {
@@ -22,22 +21,11 @@ const routes = [
             { 
                 path: "/posts/:postId", 
                 element: <Post />,
-                loader: postLoader 
+                loader: postLoader
             },
             { 
-                path: "/posts/:postId/edit", 
-                element: <PostEdit />,
-                loader: postLoader,
-                action: postUpdateAction 
-            },
-            { 
-                path: "/posts/:postId/delete", 
-                action: postDeleteAction 
-            },
-            { 
-                path: "/posts/create", 
-                element: <PostEdit />,
-                action: postCreateAction
+                path: "/posts/:postId/comments/create", 
+                action: commentCreateAction 
             },
         ],
     },

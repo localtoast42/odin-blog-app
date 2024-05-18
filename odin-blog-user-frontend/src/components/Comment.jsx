3a2f8633c38b23c9228b9ca552158eb36922531e
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-18 15:59:21 -0400

    Add edit, delete btns for author of comment

--- odin-blog-user-frontend/src/components/Comment.jsx
+++ odin-blog-user-frontend/src/components/Comment.jsx
@@ -1,18 +1,31 @@
 import "./Comment.css";
 import PropTypes from 'prop-types';
 
-const Comment = ({ comment }) => {
+const Comment = ({ comment, isAuthor }) => {
     
     return (
-        <div>
-            <h2>{comment.author.firstName}</h2>
-            <p>{comment.text}</p>
+        <div className="comment-container">
+            <div className="comment-header">
+                <div className="comment-info">
+                    <h3 className="comment-author">{comment.author.username}</h3>
+                    <h3 className="comment-date">{comment.postDateFormatted}</h3>
+                
+                </div>
+                {isAuthor && (
+                    <div className="comment-btns">
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </div>
+                )}
+            </div>
+            <p className="comment-text">{comment.text}</p>
         </div>
     )
 }
 
 Comment.propTypes = {
-    comment: PropTypes.object.isRequired
+    comment: PropTypes.object.isRequired,
+    isAuthor: PropTypes.bool.isRequired
 }
 
 export default Comment;

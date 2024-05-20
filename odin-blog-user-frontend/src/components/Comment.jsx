20fa6f527ad2cb08e88d247a1d1062acd37dc2f6
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-20 16:18:00 -0400

    Make comments editable, deletable

--- odin-blog-user-frontend/src/components/Comment.jsx
+++ odin-blog-user-frontend/src/components/Comment.jsx
@@ -1,7 +1,14 @@
 import "./Comment.css";
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { Form } from "react-router-dom";
 
 const Comment = ({ comment, isAuthor }) => {
+    const [isEditable, setIsEditable] = useState(false);
+
+    function toggleEditable() {
+        setIsEditable(!isEditable);
+    }
     
     return (
         <div className="comment-container">
@@ -9,16 +16,44 @@ const Comment = ({ comment, isAuthor }) => {
                 <div className="comment-info">
                     <h3 className="comment-author">{comment.author.username}</h3>
                     <h3 className="comment-date">{comment.postDateFormatted}</h3>
-                
                 </div>
                 {isAuthor && (
-                    <div className="comment-btns">
-                        <button>Edit</button>
-                        <button>Delete</button>
+                    <div>
+                        {!isEditable && <button onClick={toggleEditable}>Edit</button>}
+                        {isEditable && 
+                            <Form 
+                                method="post" 
+                                action={`comments/${comment.id}/delete`}
+                                onSubmit={(event) => {
+                                    if (
+                                        !confirm("Do you really want to delete this comment?")
+                                    ) {
+                                        event.preventDefault();
+                                    }
+                                }}
+                            >
+                                <button>Delete</button>
+                            </Form>
+                        }
                     </div>
                 )}
             </div>
-            <p className="comment-text">{comment.text}</p>
+            {!isEditable && <p className="comment-text">{comment.text}</p>}
+            {isEditable && 
+                <Form method="post" action={`comments/${comment.id}/edit`} className="comment-edit">
+                    <textarea
+                        name="text"
+                        id="comment-text"
+                        placeholder="Write your comment..."
+                        rows="4"
+                        defaultValue={comment.text ? comment.text : ''}
+                        className="text-input">
+                    </textarea>
+                    <div className="comment-btns">
+                        <button type="submit">Save</button>
+                        <button type="button" onClick={toggleEditable}>Cancel</button>
+                    </div>
+                </Form>}
         </div>
     )
 }

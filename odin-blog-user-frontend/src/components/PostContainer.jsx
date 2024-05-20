3a8bad37487cc36e5a6d8dc7bec0bdd2721fe6ba
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-20 13:03:07 -0400

    Remove Create Post button from reader view

--- odin-blog-user-frontend/src/components/PostContainer.jsx
+++ odin-blog-user-frontend/src/components/PostContainer.jsx
@@ -1,6 +1,6 @@
 import "./PostContainer.css";
 import PostCard from "./PostCard";
-import { Form, useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
 const PostContainer = () => {
     const { posts } = useLoaderData();
@@ -19,9 +19,6 @@ const PostContainer = () => {
             <div className="sidebar">
                 <h2>Description</h2>
                 <p>This is my blog, check it out!</p>
-                <Form method="POST" action="posts/create">
-                    <button type="submit" className="post-btn">Create New Post</button>
-                </Form>
             </div>
         </div>
     );
